c38ab56540bd397632f8a21b35130ef24063261e
EvilTeacher <59510685+EvilTeacher@users.noreply.github.com>
2021-05-04 12:14:59 +0000

    Путь до библиотеки заменен на относительный

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import getpass
 import ctypes
 
-md5_c = ctypes.CDLL('/home/dasha/University/Crypto/Kursach/code/crypto/md5.so')
+md5_c = ctypes.CDLL('./libcrypto.so')
 md5_c_func = lambda input: md5_c.md5(ctypes.c_char_p(input.encode('utf-8')))
 
 #email = input()
